b8fb37120de2490b790640b943f697cde6c031c8
AuroreMle <141311171+AuroreMle@users.noreply.github.com>
2024-09-15 04:17:23 +0200

    Mise à jour pour la gestion dynamique du titre et de la meta description

--- ttpjc/src/App.vue
+++ ttpjc/src/App.vue
@@ -9,6 +9,8 @@
 </template>
 
 <script>
+import { onMounted, watch } from 'vue'
+import { useRoute } from 'vue-router'
 import AppHeader from './components/AppHeader.vue'
 import AppFooter from './components/AppFooter.vue'
 
@@ -17,6 +19,35 @@ export default {
   components: {
     AppHeader,
     AppFooter
+  },
+  setup() {
+    const route = useRoute()
+
+    // Mettre à jour le titre lorsque la route change
+    watch(() => route.meta.title, (newTitle) => {
+      if (newTitle) {
+        document.title = newTitle
+      } else {
+        document.title = 'Titre par défaut' // Titre par défaut si aucun titre n'est spécifié
+      }
+    }, { immediate: true })
+
+    // Mettre à jour la meta description
+    watch(() => route.meta.description, (newDescription) => {
+      const metaTag = document.querySelector('meta[name="description"]')
+      if (newDescription) {
+        if (metaTag) {
+          metaTag.setAttribute('content', newDescription)
+        } else {
+          const newMetaTag = document.createElement('meta')
+          newMetaTag.setAttribute('name', 'description')
+          newMetaTag.setAttribute('content', newDescription)
+          document.head.appendChild(newMetaTag)
+        }
+      } else if (metaTag) {
+        metaTag.remove()
+      }
+    }, { immediate: true })
   }
 }
 </script>
